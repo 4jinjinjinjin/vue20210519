b42d3fa9673173f1272bece1643a41f069f5b9e2
金鑫 <963141772@qq.com>
2021-06-04 01:13:01 +0800

    会员新增

--- src/views/theFog/Member.vue
+++ src/views/theFog/Member.vue
@@ -1,34 +1,149 @@
 <template>
   <div>
+    <div style="display: flex">
+      <el-input  style="width:320px;margin-left: 5px" placeholder="请输入检索内容"
+                 v-model="search" >
+        <el-button @click="doRefresh" slot="append" icon="el-icon-refresh"></el-button>
+      </el-input>
 
-    <el-table :data="tableData">
-      <el-table-column prop="date" label="日期" width="140">
+      <el-button-group>
+        <el-button type="primary" icon="el-icon-plus" style="margin-left: 5px" @click="addVisible=true">新增会员</el-button>
+      </el-button-group>
+
+    </div>
+
+    <el-table :data="memberData.filter(data => !search || new String(data.id).includes(search) || data.name.toLowerCase().includes(search.toLowerCase()))"
+              :border=true style="margin-top: 5px;" height="800px">
+<!--      <el-table-column-->
+<!--          type="index"-->
+<!--          width="50">-->
+<!--      </el-table-column>-->
+      <el-table-column prop="id" label="编号" width="140" align="center" header-align="center">
       </el-table-column>
-      <el-table-column prop="name" label="姓名" width="120">
+      <el-table-column
+          label="姓名"
+          width="180"  align="center" header-align="center">
+        <template slot-scope="scope">
+          <el-popover trigger="hover" placement="top">
+            <p><span style="font-weight: bolder">姓名：</span>{{ scope.row.name }}</p>
+            <p><span style="font-weight: bolder">性别：</span>{{ scope.row.sex=='1'?'男':'女' }}</p>
+            <p><span style="font-weight: bolder">住址：</span>{{ scope.row.address }}</p>
+            <p><span style="font-weight: bolder">备注：</span>{{ scope.row.remark }}</p>
+
+            <div slot="reference" class="name-wrapper">
+              <el-tag  :type="scope.row.sex=='1'?'':'danger'">{{ scope.row.name }}</el-tag>
+            </div>
+          </el-popover>
+        </template>
       </el-table-column>
-      <el-table-column prop="address" label="地址">
+      <el-table-column width="240" prop="phone" label="联系电话"  align="right" header-align="center">
+      </el-table-column>
+      <el-table-column width="240" prop="opertime"  label="注册时间" align="center" header-align="center" sortable>
+      </el-table-column>
+      <el-table-column width="240" prop="balance" :formatter="formatterBalance" style="font-weight: bolder" label="余额" align="right" header-align="center" sortable>
+      </el-table-column>
+
+      <el-table-column label="操作" header-align="center" align="right" >
+        <template slot-scope="scope">
+          <el-button
+              size="mini"
+              type="success"
+              @click="handleEdit(scope.$index, scope.row)">余额充值</el-button>
+          <el-button
+              size="mini"
+              type="primary"
+              @click="handleEdit(scope.$index, scope.row)">充值记录</el-button>
+          <el-button
+              size="mini"
+              type="warning"
+              @click="handleEdit(scope.$index, scope.row)">消费记录</el-button>
+          <el-button
+              size="mini"
+              type="info"
+              @click="handleDelete(scope.$index, scope.row)">设置</el-button>
+        </template>
       </el-table-column>
     </el-table>
 
-
+    <transition name="el-zoom-in-center">
+      <el-dialog v-if="addVisible" title="付款界面" width="600px" :visible.sync="addVisible" :before-close="addClose"  >
+        <Add></Add>
+      </el-dialog>
+    </transition>
   </div>
 </template>
 
 <script>
+import Add from "@/views/theFog/member/Add";
 export default {
+  components: {
+    Add,
+  },
   name: "Member",
   methods: {
+    formatterBalance:function (row, column, cellValue, index){
+      return Number(cellValue).toFixed(2);
+    },
+    doRefresh:function () {
+      alert('刷新内容')
+      const item = {
+        id:111,
+        name:'Adds',
+        sex:1,//1 男，2 女
+        phone: 15258610732,
+        address: '上海市普陀区金沙江路 1518 弄',
+        remark:'测试备注',
+        opertime:'2021-05-30',
+        balance:500.00,
+      };
+      this.memberData=new Array(item);
+    },
+
+    addClose(done) {
+      let _this= this;
+      this.$confirm('是否还有未保存的工作?确定关闭吗？')
+          .then(_ => {
+            _this.doRefresh();
+            _this.addVisible=false;
+            done();
+          })
+          .catch(_ => {});
+    },
   },
-
-  data() {
-
+  created() {
     const item = {
-      date: '2016-05-02',
-      name: '王xxxxx',
-      address: '上海市普陀区金沙江路 1518 弄'
+      id:111,
+      name:'Adds',
+      sex:1,//1 男，2 女
+      phone: 15258610732,
+      address: '上海市普陀区金沙江路 1518 弄',
+      remark:'测试备注',
+      opertime:'2021-05-30',
+      balance:500.00,
     };
+    this.memberData.push(item);
+
+    for (let i=0;i<20;i++){
+      const item = {
+        id:i+1,
+        name:'测试'+i,
+        sex:i%2+1,//1 男，2 女
+        phone: 15258610732,
+        address: '上海市普陀区金沙江路 1518 弄',
+        remark:'测试备注'+i,
+        opertime:'2021-05-30',
+        balance:500.00+i,
+      };
+      this.memberData.push(item);
+    }
+  },
+  data() {
     return {
-      tableData: Array(20).fill(item),
+      search:'',
+      addVisible:false,//结算界面
+
+
+      memberData: [],
       nowWeek: '',
       nowDate: '',
       nowTime: '',
---
+++ src/views/theFog/member/Add.vue
@@ -0,0 +1,82 @@
+<template>
+  <div>
+    <el-form ref="addMember" :rules="rules" :model="member" label-width="80px" style="width: 500px" >
+      <el-form-item label="会员姓名"  prop="name">
+        <el-input v-model="member.name" ></el-input>
+      </el-form-item>
+      <el-form-item label="会员性别" prop="sex">
+        <el-select v-model="member.sex" placeholder="请选择会员性别">
+          <el-option label="男" value="1"></el-option>
+          <el-option label="女" value="2"></el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item label="联系方式" prop="phone">
+        <el-input v-model.number="member.phone"></el-input>
+      </el-form-item>
+      <el-form-item label="联系住址">
+        <el-input v-model="member.address"></el-input>
+      </el-form-item>
+      <el-form-item label="备注">
+        <el-input v-model="member.remark"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="onSubmit('addMember')">立即创建</el-button>
+        <el-button @click="cancleClick">取消</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Add",
+  props: {
+    payRoomId: Number,
+    payVisible: Boolean
+  },
+
+  data() {
+    return {
+      member: {
+        id: '',
+        sex: '',
+        phone: '',
+        address: '',
+        remark: '',
+      },
+      rules: {
+        name: [
+          { required: true, message: '请输入会员名称', trigger: 'blur' },
+        ],
+        sex: [
+          { required: true, message: '请选择会员性别', trigger: 'change' }
+        ],
+        phone: [
+          { required: true, message: '请输入联系方式', trigger: 'change' },
+        ],
+      }
+    }
+  },
+  methods: {
+    cancleClick(){
+      this.$parent.$parent.addClose();
+    },
+    onSubmit(formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          //开始新增
+          this.$message({
+            message: '新增成功！',
+            type: 'success'
+          });
+          this.$parent.$parent._data.addVisible=false;
+        }
+      });
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
